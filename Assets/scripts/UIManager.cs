fe4640864ffc41db54b098d7237856094bbffef6
Navya Nandini <navyanandini2222@gmail.com>
2026-05-11 00:38:51 +0530

    ui updated

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -26,7 +26,7 @@ public class UIManager : MonoBehaviour
 
   void Start()
   {
-    ShowPanel(homePanel);
+    ShowPanel(startPanel);
   }
 
   // -------------------------------------------------------------------
